83e9c3ff01374d7034b39e1e26ab1df0b393f801
mohammednudman <mohammednudman@gmail.com>
2023-03-19 08:13:34 +0530

    Database Error Solved

--- AdminDashboard/backend/config/dbConnection.js
+++ AdminDashboard/backend/config/dbConnection.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log(
-      "Database connected:",
-      connect.connection.host,
-      connect.connection.name
-    );
+    mongoose
+      .connect(process.env.CONNECTION_STRING, {
+        useUnifiedTopology: true,
+      })
+      .then(() =>
+        console.log(
+          "==============Mongodb Database Connected Successfully=============="
+        )
+      )
+      .catch((err) => console.log(err));
   } catch (err) {
     console.log(err);
     process.exit(1);
